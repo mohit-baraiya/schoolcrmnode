644b0092b70669df68cd63c93fdbddf35b5e40ca
mohit-baraiya <105412007+mohit-baraiya@users.noreply.github.com>
2022-07-23 17:37:26 +0530

    faculty create and session set

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const session = require('express-session')
 const app = express();
 const port = 5000;
 require("./configs/dbConnect");
+app.use(session({
+    secret: 'secret session',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
+session.s
 app.use(express.urlencoded({ extended: true }));
 app.use("/", require("./routes/"));
 
